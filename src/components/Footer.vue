f8f4cf48b41639aa04918d46f5d2e6defef5255a
Ryan <ryan.dsilva.98@gmail.com>
2018-08-28 18:17:50 +0530

    Added Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,40 @@
 <template>
-  <div class="footer">
-    Footer
-  </div>
+  <v-footer height="auto" color="indigo">
+    <v-layout justify-center row wrap>
+      <v-btn v-for="link in links" :key="link.name" color="white" :to="link.href" flat round>
+        {{ link.name }}
+      </v-btn>
+      <v-flex indigo darken-1 py-3 text-xs-center white--text xs12>
+        &copy;2018 —
+        <strong>Ryan Dsilva | Niharika Gogate | Katoushka Gracias</strong>
+      </v-flex>
+    </v-layout>
+  </v-footer>
 </template>
 
 <script>
 export default {
   name: 'Footer',
+  data: () => ({
+    links: [
+      {
+        name: 'Home',
+        href: '/',
+      },
+      {
+        name: 'Users',
+        href: '/user',
+      },
+      {
+        name: 'Council',
+        href: '/council',
+      },
+      {
+        name: 'GitHub',
+        href: '#',
+      },
+    ],
+  }),
 };
 </script>
 
