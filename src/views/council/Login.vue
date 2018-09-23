865e6067bafda0ec215d5b1144af5f284379b0d6
Ryan <ryan.dsilva.98@gmail.com>
2018-09-24 01:40:08 +0530

    Added Council Files

---
+++ src/views/council/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="council-login"></div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+};
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/views/council/SignUp.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="council-signup"></div>
+</template>
+
+<script>
+export default {
+  name: 'Signup',
+};
+</script>
+
+<style scoped lang="scss">
+</style>
