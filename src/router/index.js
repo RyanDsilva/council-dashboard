e8095d10f9dbcf8ca000be9536849c3b2e49f8ba
Ryan <ryan.dsilva.98@gmail.com>
2018-09-23 17:14:39 +0530

    Changed User and Router

--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,13 @@ import NewEvent from '@/views/event/NewEvent';
 import AllEvents from '@/views/event/AllEvents';
 import EditEvent from '@/views/event/EditEvent';
 import EventInfo from '@/views/event/EventInfo';
-import Login from '@/views/user/Login';
+import UserCreate from '@/views/user/UserCreate';
+import UserDashboard from '@/views/user/UserDashboard';
 import Error404 from '@/views/404';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -21,6 +22,9 @@ export default new Router({
       path: '/event/create',
       name: 'NewEvent',
       component: NewEvent,
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
       path: '/event/all',
@@ -36,11 +40,23 @@ export default new Router({
       path: '/event/:id/edit',
       name: 'EditEvent',
       component: EditEvent,
+      meta: {
+        requiresAuth: true,
+        is_admin: true,
+      },
     },
     {
-      path: '/user/login',
-      name: 'Login',
-      component: Login,
+      path: '/user/create',
+      name: 'UserCreate',
+      component: UserCreate,
+    },
+    {
+      path: '/user/:rollNo',
+      name: 'UserDashboard',
+      component: UserDashboard,
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
       path: '*',
@@ -49,3 +65,29 @@ export default new Router({
     },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (localStorage.getItem('user') == null) {
+      next({
+        path: '/login',
+        params: { nextUrl: to.fullPath },
+      });
+    } else {
+      const user = JSON.parse(localStorage.getItem('user'));
+      if (to.matched.some(record => record.meta.is_admin)) {
+        if (user.isAdmin) {
+          next();
+        } else {
+          next({ name: 'AllEvents' });
+        }
+      } else {
+        next();
+      }
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
