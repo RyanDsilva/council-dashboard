8f7c171289706467962fd18f350445bbe2c4dceb
Ryan <ryan.dsilva.98@gmail.com>
2018-09-24 00:18:21 +0530

    Changed Nav and Footer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,34 +1,29 @@
 <template>
   <span>
-    <v-navigation-drawer v-model="drawer" temporary dark absolute class="indigo darken-4">
+    <v-navigation-drawer v-model="drawer" temporary dark absolute class="blue-grey darken-4">
       <v-list>
         <v-list-tile class="sidebarHeader" height="auto">
           <v-list-tile-avatar tile size="50">
             <img src="../assets/logo.png">
           </v-list-tile-avatar>
           <v-list-tile-content>
-            <v-list-tile-title class="headline">{{council}}</v-list-tile-title>
+            <v-list-tile-title class="council-name">{{council}}</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
       <v-list>
         <v-divider class="mb-5"></v-divider>
-        <v-list-tile to="/">
+        <v-list-tile to="/" class="side-links">
           <v-list-tile-content>
             <v-list-tile-title>Home</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
-        <v-list-tile to="/user/login">
-          <v-list-tile-content>
-            <v-list-tile-title>User Login</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile to="/council/login">
+        <v-list-tile to="/login" class="side-links">
           <v-list-tile-content>
             <v-list-tile-title>Council Login</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
-        <v-list-tile to="/event/all">
+        <v-list-tile to="/event/all" class="side-links">
           <v-list-tile-content>
             <v-list-tile-title>Events</v-list-tile-title>
           </v-list-tile-content>
@@ -42,8 +37,8 @@
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn to="/user/login" flat>User Login</v-btn>
-        <v-btn to="/council/login" flat>Council Login</v-btn>
+        <v-btn v-if="!isLoggedIn" to="/login" flat>Council Login</v-btn>
+        <v-btn v-if="isLoggedIn" :to="makeLink" flat>Dashboard</v-btn>
         <v-btn to="/event/all" flat>Events</v-btn>
       </v-toolbar-items>
     </v-toolbar>
@@ -51,19 +46,28 @@
 </template>
 
 <script>
+import mapState from 'vuex';
+
 export default {
   name: 'Navbar',
   // eslint-disable-next-line
   data: () => {
     return {
-      drawer: null,
-      council: 'StuCo',
+      drawer: false,
+      council: 'CSI',
     };
+  },
+  computed: {
+    ...mapState[('user', 'isLoggedIn')],
+    makeLink() {
+      // eslint-disable-next-line
+      return '/council' + this.$store.state.user._id + '/dashboard';
+    },
   },
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .main a {
   text-decoration: none;
   color: white;
@@ -71,7 +75,10 @@ export default {
 .sidebarHeader {
   margin: 10px;
 }
-.headline {
-  height: auto;
+.council-name {
+  line-height: auto;
+  font-weight: 700;
+  font-size: 1.9em;
+  margin-left: 0.5em;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,8 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
-    user: !!localStorage.getItem('user'),
+    user: localStorage.getItem('user'),
+    isLoggedIn: !!localStorage.getItem('user'),
   },
 });
 
