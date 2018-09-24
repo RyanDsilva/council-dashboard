d1eafcb1fc69ec08019425c775ac4b1795bffa1a
Ryan <ryan.dsilva.98@gmail.com>
2018-09-24 10:36:29 +0530

    Added Login and Signup

--- src/views/council/Login.vue
+++ src/views/council/Login.vue
@@ -1,12 +1,59 @@
 <template>
-  <div class="council-login"></div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 class="text-xs-center">
+        <div class="council-login">
+          <v-card raised ripple class="mx-auto my-5 card-wrapper">
+            <v-card-text class="form-content">
+              <h1 class="login-title">Council Login</h1>
+              <v-form v-model="valid">
+                <v-text-field v-model="username" type="text" label="Username" required></v-text-field>
+                <v-text-field v-model="password" type="password" label="Password" required></v-text-field>
+                <v-btn color="indigo" dark @click.prevent="login">Login</v-btn>
+              </v-form>
+              <p class="my-2">Not Already A User?
+                <router-link class="reg-link" to="/signup">Register Now!</router-link>
+              </p>
+            </v-card-text>
+          </v-card>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Login',
+  data: () => ({
+    username: '',
+    password: '',
+  }),
+  methods: {
+    login() {
+      alert('Login Clicked!');
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
+.login-title {
+  font-weight: 700;
+  color: #39499f;
+}
+.reg-link {
+  text-decoration: none;
+}
+.card-wrapper {
+  width: 80%;
+  padding: 3em;
+}
+@media (min-width: 992px) {
+  .card-wrapper {
+    width: 50%;
+  }
+}
 </style>
--- src/views/council/SignUp.vue
+++ src/views/council/SignUp.vue
@@ -1,12 +1,80 @@
 <template>
-  <div class="council-signup"></div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 class="text-xs-center">
+        <div class="council-reg">
+          <v-card raised class="mx-auto my-5 card-wrapper">
+            <v-card-text class="form-content">
+              <h1 class="reg-title">Council Register</h1>
+              <v-form>
+                <v-text-field v-model="username" type="text" label="Username" required></v-text-field>
+                <v-text-field v-model="name" type="text" label="Name Of The Council" required></v-text-field>
+                <v-text-field v-model="password" type="password" label="Password" required></v-text-field>
+                <v-textarea v-model="description" label="Council Description" hint="Detailed Council Description preffered."></v-textarea>
+                <v-btn color="indigo" dark @click.prevent="signup">Register</v-btn>
+              </v-form>
+              <p class="my-2">Already A User?
+                <router-link class="log-link" to="/login">Login!</router-link>
+              </p>
+            </v-card-text>
+          </v-card>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: 'Signup',
+  name: 'SignUp',
+  data: () => ({
+    username: '',
+    password: '',
+    name: '',
+    description: '',
+    error: '',
+  }),
+  methods: {
+    signup() {
+      axios
+        .post('/council/register', {
+          username: this.username,
+          password: this.password,
+          name: this.name,
+          description: this.description,
+        })
+        .then(res => {
+          const data = res.data;
+          this.$session.start();
+          this.$session.set('user', data);
+          localStorage.setItem('user', data);
+          this.$router.push('/event/all');
+        })
+        .catch(err => {
+          this.error = err.message;
+        });
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
+.reg-title {
+  font-weight: 700;
+  color: #39499f;
+}
+.log-link {
+  text-decoration: none;
+}
+.card-wrapper {
+  width: 80%;
+  padding: 3em;
+}
+@media (min-width: 992px) {
+  .card-wrapper {
+    width: 50%;
+  }
+}
 </style>
