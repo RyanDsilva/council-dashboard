820b54e94f3e58a08ed6aedb2ff612dc22932342
Ryan <ryan.dsilva.98@gmail.com>
2018-09-30 23:21:56 +0530

    ReDesigned Home Page

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,6 +1,33 @@
 <template>
-  <div id="index">
-    <v-img class="heroSVG ml-auto" src="../assets/svg/collab.svg"></v-img>
+  <div>
+    <section id="hero-kollab">
+      <v-container>
+        <v-layout row wrap>
+          <v-flex sm12 md6>
+            <div id="title-heading">
+              <h1>KOLLAB</h1>
+              <h3 class="subheading">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, perferendis? Iure quam ducimus, a nam quia quasi nemo illo harum nihil maiores corrupti molestias ipsum aliquid quo! Dolor, quae molestiae!</h3>
+              <br>
+              <v-btn to="/event/all" large color="deep-purple darken-1" raised dark>Search For Interesting Events!</v-btn>
+              <br>
+          </div>
+          </v-flex>
+          <v-flex sm12 md6>
+            <v-img src="../assets/svg/hero.svg"></v-img>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </section>
+    <br>
+    <section id="kollab-content">
+      <v-container grid-list-md>
+        <v-layout row wrap>
+          <v-flex xs12>
+            <h1>Test Content</h1>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </section>
   </div>
 </template>
 
@@ -11,11 +38,30 @@ export default {
 </script>
 
 <style scoped lang="scss">
-#index {
-  background-color: #bedee5;
+#hero-kollab {
+  background-image: url('../assets/img/welcome-bg.png');
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  padding-bottom: 10em;
 }
-.heroSVG {
-  width: 50%;
-  height: 50%;
+#title-heading {
+  margin-top: 9em;
+  text-align: center;
+  h1 {
+    font-size: 5em;
+    color: #ffffff;
+    padding: 0.1rem;
+  }
+  .subheading {
+    color: #e4e4e4;
+  }
+}
+@media only screen and (max-width: 600px) {
+  #title-heading {
+    margin: auto;
+  }
+}
+#kollab-content {
 }
 </style>
