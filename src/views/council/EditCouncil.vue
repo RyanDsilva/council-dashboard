e819e432ee203e9b4ec7630f441d56656007b455
Ryan <ryan.dsilva.98@gmail.com>
2018-10-11 23:57:36 +0530

    Edit Council

--- src/views/council/EditCouncil.vue
+++ src/views/council/EditCouncil.vue
@@ -8,7 +8,7 @@
             <v-text-field v-model="council.name" label="Name of the Council" required></v-text-field>
             <v-textarea v-model="council.description" label="Council Description" hint="Detailed Council Description preffered."></v-textarea>
             <div class="my-2 mx-auto">
-              <v-btn @click="submit" color="indigo" dark>Edit Council</v-btn>
+              <v-btn @click="submit" color="indigo" dark>Confirm Edit</v-btn>
             </div>
           </v-form>
         </v-flex>
@@ -18,7 +18,41 @@
 </template>
 
 <script>
+import axios from 'axios';
 
+export default {
+  data: () => ({
+    council: {},
+  }),
+  methods: {
+    submit() {
+      axios
+        // eslint-disable-next-line
+        .put('/council/' + this.$route.params.id + '/edit', {
+          council: this.council,
+        })
+        .then(
+          // eslint-disable-next-line
+          this.$router.push('/council/' + this.$route.params.id + '/dashboard'),
+        )
+        .catch(err => {
+          this.error = err.message;
+        });
+    },
+  },
+  created() {
+    axios
+      // eslint-disable-next-line
+      .get('/council/' + this.$route.params.id + '/dashboard')
+      .then(res => {
+        const data = res.data;
+        this.council = data;
+      })
+      .catch(err => {
+        this.error = err.message;
+      });
+  },
+};
 </script>
 
 <style scoped lang="scss">
