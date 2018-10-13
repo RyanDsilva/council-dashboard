08ca838bc2d3e80a149f3c57cd8e8ecdfeaacb9f
Ryan <ryan.dsilva.98@gmail.com>
2018-10-13 11:47:07 +0530

    Changed User Dashboar

--- src/views/user/UserCreate.vue
+++ src/views/user/UserCreate.vue
@@ -1,86 +1,85 @@
 <template>
-  <div id="user-add" >
-   <v-container>
+  <v-container>
     <v-layout row wrap>
       <v-flex xs12 class="text-xs-center">
-      <v-card raised ripple class="mx-auto my-5 card-wrapper">
+        <div class="council-reg">
+          <v-card raised class="mx-auto my-5 card-wrapper">
             <v-card-text class="form-content">
-              <h1 class="header">Register</h1>
+              <h1 class="reg-title">User Register</h1>
+              <p>In order to register for any events, you will have to be a registered user.</p>
               <v-form>
-                <v-text-field v-model="name" type="text" label="Username" required></v-text-field>
-                <v-text-field v-model="email" type="email" label="Email" required></v-text-field>
-                <v-text-field v-model="phone" type="number" label="Contact" required></v-text-field>
-                <v-text-field v-model="rollno" type="number" label="Roll No" required></v-text-field>
-                
-                <v-flex xs14 sm10 d-flex >
-                  <v-select class="year"
-                      :items="items"
-                      label="Year"
-                   ></v-select>
-                
-                  <v-select class="branch"
-                      :items="options"
-                      label="Branch"
-                   ></v-select>
-             
-                </v-flex>
-                
-          
-                
-                <v-btn class="banana" color="indigo" dark>Done</v-btn>
-                
+                <v-text-field v-model="rollNo" type="number" label="Roll No." required></v-text-field>
+                <v-text-field v-model="name" type="text" label="Full Name" required></v-text-field>
+                <v-text-field v-model="email" type="email" label="Email Address" required></v-text-field>
+                <v-text-field v-model="phone" type="text" label="Contact Number"></v-text-field>
+                <v-select :items="years" label="Year" v-model="year"></v-select>
+                <v-select :items="branches" label="Branch" v-model="branch"></v-select>
+                <v-btn color="indigo" dark @click.prevent="userCreate">Register</v-btn>
               </v-form>
-              
             </v-card-text>
-      </v-card>
-       
+          </v-card>
+        </div>
       </v-flex>
     </v-layout>
-  </v-container>                                                                                                                                                                                                                                                                            o                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
-  </div>
-
+  </v-container>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    data: () => ({
-      items: ['FE', 'SE', 'TE', 'BE'],
-      options: ['CS','IT','ELEX','PROD'],
-      name: '',
-      email:'',
-      phone:'',
-      rollno:'',
-
-    })
-  }
-
-
+  name: 'UserCreate',
+  data: () => ({
+    rollNo: '',
+    phone: '',
+    name: '',
+    email: '',
+    year: '',
+    branch: '',
+    error: '',
+    years: [
+      'First Year (FE)',
+      'Second Year (SE)',
+      'Third Year (TE)',
+      'Final Year (BE)',
+    ],
+    branches: ['Computers', 'Info. Tech.', 'Electronics', 'Production'],
+  }),
+  methods: {
+    userCreate() {
+      axios
+        .post('/user/create', {
+          rollNo: this.rollNo,
+          email: this.email,
+          name: this.name,
+          phone: this.phone,
+          year: this.year,
+          branch: this.branch,
+        })
+        .then(this.$router.push('/event/all'))
+        .catch(err => {
+          this.error = err.message;
+        });
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
-.header{
+.reg-title {
   font-weight: 700;
   color: #39499f;
-
 }
-
+.log-link {
+  text-decoration: none;
+}
 .card-wrapper {
   width: 80%;
   padding: 3em;
 }
-
 @media (min-width: 992px) {
   .card-wrapper {
     width: 50%;
   }
 }
-
-.year{
-  padding-left: 5em;
-}
-
-.branch{
-  padding-left: 3em;
-}
-
 </style>
